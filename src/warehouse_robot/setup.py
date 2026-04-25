9c06a1e859e45bb81c185952c8646c782ce1f7f7
bigboss <legojaddai@gmail.com>
2026-04-25 23:46:25 +0300

    feat: Implement delivery action client and server with safety monitor
    - Added delivery_action_client and delivery_action_server to handle package delivery actions.
    - Introduced safety_monitor_node to monitor robot safety and manage emergency stop conditions.
    - Updated setup.py to include new nodes and their configurations.
    - Created launch files for orchestrating the execution of all nodes.
    - Enhanced environment setup with necessary paths for custom interfaces.
    - Updated package metadata and sources for better clarity and organization.

--- src/warehouse_robot/setup.py
+++ src/warehouse_robot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'warehouse_robot'
 
@@ -9,26 +11,31 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*')),
+
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bigboss',
     maintainer_email='bigboss@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Warehouse robot package',
+    license='TODO',
     extras_require={
-        'test': [
-            'pytest',
-        ],
+        'test': ['pytest'],
     },
     entry_points={
-    'console_scripts': [
-        'sensor_node = warehouse_robot.sensor_node:main',
-        'task_node = warehouse_robot.task_node:main',
-        'delivery_action_server = warehouse_robot.delivery_action_server:main',
-        'delivery_action_client = warehouse_robot.delivery_action_client:main',
-        'safety_monitor_node = warehouse_robot.safety_monitor_node:main'
-    ],
-},
+        'console_scripts': [
+            'sensor_node = warehouse_robot.sensor_node:main',
+            'task_node = warehouse_robot.task_node:main',
+            'delivery_action_server = warehouse_robot.delivery_action_server:main',
+            'delivery_action_client = warehouse_robot.delivery_action_client:main',
+            'safety_monitor_node = warehouse_robot.safety_monitor_node:main'
+        ],
+    },
 )
